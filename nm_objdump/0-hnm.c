8714eb6812f3708c316da019d2a9a6ba5ff28aa6
FrankOnyemaOrji <f.orji@alustudent.com>
2024-08-02 18:14:25 +0200

    New changes to nm_obj_dump

---
+++ nm_objdump/0-hnm.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+
+/**
+ * main - entry
+ * @argc: args count
+ * @argv: args
+ * @env: enviroment
+ * Author: Frank Onyema Orji
+ * Return: exit or fail
+ */
+int main(int argc, char **argv, char **env)
+{
+	char *comm[] = {"./hnm", "-p", "", NULL};
+
+	(void)argc;
+
+	comm[2] = argv[1];
+	if (execve("/usr/bin/nm", comm, env) == -1)
+	{
+		perror("execv");
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
+}
